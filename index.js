4185a2d52a6626f01c6c7daca473556e6413af98
Mike McBride <mmcbride1007@gmail.com>
2017-04-19 10:25:59 -0500

    hide title when only one tab and tweak nav styles

--- index.js
+++ index.js
@@ -32,12 +32,11 @@ const colors = {
 module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark')
 
 module.exports.decorateConfig = config => {
-  const transparencyValue = config.transparentBgAlpha || 1
-  const backgroundColor = transparencyValue === 1 ? black : `rgba(33,40,54,${transparencyValue})`
+  const backgroundColor = black
   const foregroundColor = white
   const cursorColor = config.cursorColor || '#528bff'
   const borderColor = '#141820'
-  const inactiveTabBg = '#1b212c'
+  const tabNavBg = '#1b212c'
   const tabText = 'rgba(153,163,184,.8)'
   const tabTextActive = '#d5d9e2'
   const dividerBg = 'rgba(64,74,89,.4)'
@@ -56,9 +55,16 @@ module.exports.decorateConfig = config => {
     `,
     css: `
       ${config.css || ''}
+      .tabs_nav {
+        background-color: ${tabNavBg};
+        border-bottom: 1px solid ${borderColor};
+      }
       .tabs_list {
         margin-left: 0;
       }
+      .tabs_title {
+  			display: none !important;
+  		}
       .tab_tab.tab_first {
         padding-left: 82px;
       }
@@ -66,8 +72,8 @@ module.exports.decorateConfig = config => {
         color: ${tabText};
         font-weight: 500;
       }
-      .tab_tab:not(.tab_active) {
-        background-color: ${inactiveTabBg};
+      .tab_tab.tab_active {
+        background-color: ${backgroundColor};
       }
       .tab_tab.tab_active .tab_textInner {
         color: ${tabTextActive};
